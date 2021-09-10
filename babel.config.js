e324fa85b014eacad196fee978c138b9b4012d0c
Carlos Gabriel Godri <08508575971@itajai.local>
2021-09-10 15:15:47 -0300

    [chore] fisrt commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ['.'],
+      alias: {
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@constants': './src/constants',
+        '@contexts': './src/contexts',
+        '@hooks': './src/hooks',
+        '@interfaces': './src/interfaces',
+        '@routes': './src/routes',
+        '@screens': './src/screens',
+        '@services': './src/services',
+        '@utils': './src/utils',
+        '@styles': './src/global/styles',
+      },
+    },
+  ],
+  [
+    'module:react-native-dotenv',
+    {
+      moduleName: '@env',
+      path: '.env',
+      safe: false,
+      allowUndefined: true,
+      verbose: false,
+    },
+  ],
+];
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [...plugins],
 };
